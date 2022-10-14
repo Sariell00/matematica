dd39751afe2a254c87cef63ebaba6b8b37f5e26f
Sariell00 <115809337+Sariell00@users.noreply.github.com>
2022-10-14 15:57:47 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,21 @@
-from . import medidas_dispersao
+from . import Medidas_dispersao
+
+print(
+'1 - Medidas de dispersão' /n
+)
+opcao = input("Digite a opção")
+
+if opcao == '1':
+    print("Medidas de dispersão /n",
+    "Digite os valores: ")
+    valores = []
+    while True:
+        valor = input()
+        if not valor.isdigit():
+            break
+        valores.append(float(valor))
+    a = Medidas_dispersao()
+    a.media()
+    a.desvio_medio()
+    a.variancia()
+    a.desvio_padrao()
